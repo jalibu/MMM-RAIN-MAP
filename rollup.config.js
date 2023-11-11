aa9ebfc1225696d5910d43231a3ef91375cfb27f
Kristjan ESPERANTO <35647502+KristjanESPERANTO@users.noreply.github.com>
2023-11-11 06:15:31 +0100

    Load package.json without fs

--- rollup.config.js
+++ rollup.config.js
@@ -3,9 +3,7 @@ import commonjs from '@rollup/plugin-commonjs'
 import nodeResolve from '@rollup/plugin-node-resolve'
 import terser from '@rollup/plugin-terser'
 import typescript from '@rollup/plugin-typescript'
-import fs from 'fs'
-
-const pkg = JSON.parse(fs.readFileSync('./package.json', 'utf-8'))
+import pkg from './package.json' assert { type: 'json' }
 
 const bannerText = `/*! *****************************************************************************
   ${pkg.name}
