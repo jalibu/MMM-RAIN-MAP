8f9e78a16574e8cf7fc351cb3fb11ec912b11056
Kristjan ESPERANTO <35647502+KristjanESPERANTO@users.noreply.github.com>
2025-04-24 20:54:26 +0200

    fix: update import statement to use 'with' syntax

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -3,7 +3,7 @@ import commonjs from '@rollup/plugin-commonjs'
 import nodeResolve from '@rollup/plugin-node-resolve'
 import terser from '@rollup/plugin-terser'
 import typescript from '@rollup/plugin-typescript'
-import pkg from './package.json' assert { type: 'json' }
+import pkg from './package.json' with { type: 'json' }
 
 const bannerText = `/*! *****************************************************************************
   ${pkg.name}
