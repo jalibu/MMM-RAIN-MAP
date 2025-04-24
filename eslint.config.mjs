9a3d8b38d286e2c71fa6a316d0543187220ca497
Kristjan ESPERANTO <35647502+KristjanESPERANTO@users.noreply.github.com>
2025-04-24 21:31:56 +0200

    chore: fix ESLint ignore config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -6,10 +6,12 @@ export default tseslint.config(
   ...tseslint.configs.stylistic,
   ...tseslint.configs.recommended,
   {
+    ignores: ['MMM-RAIN-MAP.js']
+  },
+  {
     rules: {
       'no-empty-function': 'off',
       '@typescript-eslint/no-empty-function': 'warn'
-    },
-    ignores: ['*.js']
+    }
   }
 )
